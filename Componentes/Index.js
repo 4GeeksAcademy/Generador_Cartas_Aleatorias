99c3288a5885a30b34b12cbdc21909a2bcea083c
JorgeMRs <jocady21@gmail.com>
2023-08-03 15:30:07 -0300

    Sin_terminar

---
+++ Componentes/Index.js
@@ -0,0 +1,34 @@
+
+//cargar numero
+function cambiar(valor) {
+
+    let resultado = valor
+
+    let l_top1 = document.getElementById("inpute")
+
+    l_top1.innerHTML = resultado;
+    
+}
+
+
+const numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "k"]
+
+
+
+
+
+
+
+
+
+
+/* 
+const boton = document.getElementById("boton")
+//funcion encargada de carcar la escusa en el parrafo "escus"
+function render (){
+    console.log ("este sech")
+    const resultado = cambiar() 
+    return document.getElementById("escus").innerHTML = resultado;
+    
+    
+} */
